9eacfaf1b99b253c4dacceeb9a6f1436fc288a2d
Megha <jangidmegha2002@gmail.com>
2025-04-03 23:58:29 +0530

    react proj

--- photo_sharing/src/App.js
+++ photo_sharing/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const App = () => {
-    
+
+ return (
+    <div>
+        <p>HELLO GUYS!</p>
+        <select>
+        <option></option>
+        <option>car</option>
+        <option>bicycle</option>
+        <option>bike</option>
+        </select>
+    </div>
+ );
 
 }
 
