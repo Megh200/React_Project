5d016c9c36903253c52a6cb5c50ab38f5112b056
Megha <jangidmegha2002@gmail.com>
2025-04-08 22:17:23 +0530

    ..

--- photo_sharing/Backened/practice/app.js
+++ photo_sharing/Backened/practice/app.js
@@ -3,6 +3,16 @@ const express = require('express');
 
 const app = express();
 
+// since error "cannot GET /" ,so gave http req & res
+app.get("/", (req,res) =>(
+    res.send("server now found by routing / "))
+);
+
+// now if we want to create one more path
+app.get("/about", (req,res) =>(
+    res.send(" the about page")
+));
+
 // now to create a server we use..
 app.listen(5000, () =>{
     console.log("server's created");
