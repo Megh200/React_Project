9c8d96a67768d2041e8fcb83ee5bd0097ef40821
Megha <jangidmegha2002@gmail.com>
2025-04-12 00:10:38 +0530

    ..

--- photo_sharing/src/App.js
+++ photo_sharing/src/App.js
@@ -1,41 +1,9 @@
-import React, {useState} from "react";
-import Forum from "./forum.js" ;
-import Ad from "./ad.js";
+import React from "react";
+
 
 const App = () => {
 
     
-
-    const [array, damfun] = useState(
-          [  {name:"ama", class:2},
-            {name:"hina", class:5},
-            {name:"sik", class:3},    ]
-    );
-
-    const prin = function(newo){
-        damfun( (ia) =>{
-            return ia.concat(newo);
-        } )
-        
-    };
-
- return (
-    <div>
-        <p>HELLO GUYS!</p>
-        <select>
-        <option></option>
-        <option>car</option>
-        <option>bicycle</option>
-        <option>bike</option>
-        </select>
-
-        <Forum ar={array} />
-
-        <Ad attr={prin} />
-        
-    </div>
- );
-
 }
 
 export default App;
