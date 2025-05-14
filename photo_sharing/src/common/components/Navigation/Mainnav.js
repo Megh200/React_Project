ae7a94d913eb7f4d033b57ae88fdd22affc457cc
Megha <jangidmegha2002@gmail.com>
2025-05-14 09:59:28 +0530

    ..

--- photo_sharing/src/common/components/Navigation/Mainnav.js
+++ photo_sharing/src/common/components/Navigation/Mainnav.js
@@ -1,11 +1,21 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
+import Mainheader from "./Mainheader";
+import Navlink from "./Navlink";
 import "./Mainnav.css";
 
-const Mainnav = () =>{
-    return (<header>
+const Mainnav = (props) =>{
+    return (
+    <Mainheader>
+        <h1 className="mainnav-title" >       
+            <Link to = "/">Picpot</Link>
+            </h1>
+            <nav>
+                <Navlink/>
+            </nav>
 
-    </header>);
+    </Mainheader>);
 };
 
 export default Mainnav;
