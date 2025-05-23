14430f2b9da38e2ad7bad3eb8d913a467432ccfe
Megha <jangidmegha2002@gmail.com>
2025-05-23 20:17:37 +0530

    ..

--- photo_sharing/src/common/components/Navigation/Navlink.js
+++ photo_sharing/src/common/components/Navigation/Navlink.js
@@ -1,11 +1,26 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 import "./Navlink.css";
 
 const Navlink = (props) =>{
-    return (<header>
-
-    </header>);
+    return (
+        <ul className="navlinks" >
+            <li>
+                <NavLink to="/" exact>Users</NavLink>
+            </li>
+            <li>
+                <NavLink to="/1/locations" >My locations</NavLink>
+            </li>
+            <li>
+                <NavLink to="/loc/new" >Add locations</NavLink>
+            </li>
+            <li>
+                <NavLink to="/Login" >SignIn/Up</NavLink>
+            </li>
+            
+        </ul>
+    );
 };
 
 export default Navlink;
