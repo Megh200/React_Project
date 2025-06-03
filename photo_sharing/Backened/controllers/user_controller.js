8b5a1bdb8a26bf134990d684cff93d1129d974e6
Megha <jangidmegha2002@gmail.com>
2025-06-04 00:57:45 +0530

    ..

---
+++ photo_sharing/Backened/controllers/user_controller.js
@@ -0,0 +1,14 @@
+
+
+const all_users = [
+            {id:"1", name:"tiya", pic:"https://i.pinimg.com/736x/6a/8a/75/6a8a75997108f805112355c48ca4af27.jpg", nol:3},
+            {id:"2", name:"arya", pic:"https://i.pinimg.com/736x/6a/8a/75/6a8a75997108f805112355c48ca4af27.jpg", nol:1}
+        ]
+
+exports.getuserbyuid = (req, res, next) =>{
+    const uid = req.params.uid;
+    const user = all_users.filter(user => user.id===uid);
+
+    res.status(200).json({result:"success", msg:user});
+}
+
