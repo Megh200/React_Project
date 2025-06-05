10771d90c1fe71f9f40d6e0bfd1e9a4465e69115
Megha <jangidmegha2002@gmail.com>
2025-06-06 01:05:41 +0530

    ...

--- photo_sharing/Backened/controllers/user_controller.js
+++ photo_sharing/Backened/controllers/user_controller.js
@@ -1,4 +1,4 @@
-
+const MyError = require("../model/error");
 
 const all_users = [
             {id:"1", name:"tiya", pic:"https://i.pinimg.com/736x/6a/8a/75/6a8a75997108f805112355c48ca4af27.jpg", nol:3},
@@ -9,6 +9,9 @@ exports.getuserbyuid = (req, res, next) =>{
     const uid = req.params.uid;
     const user = all_users.filter(user => user.id===uid);
 
+    if(!user){
+        next(new Myerror("no such userid", 404));
+    }
     res.status(200).json({result:"success", msg:user});
 }
 
