87184a611ce7fd27402cc320fab7e5e5e98a2940
Megha <jangidmegha2002@gmail.com>
2025-06-10 21:15:21 +0530

    ..

--- photo_sharing/Backened/practice/app.js
+++ photo_sharing/Backened/practice/app.js
@@ -3,17 +3,20 @@ const express = require('express');
 
 const app = express();
 
-// since error "cannot GET /" ,so gave http req & res
+// a http method since error "cannot GET /" ,so gave http req & res
 app.get("/", (req,res) =>(
-    res.send("<h1>server now found by routing / </h1>"))
-);
+    // res.send("<h1>server now found by routing / </h1>")
 
-// now if we want to create one more path
-app.get("/about", (req,res) =>(
-    res.send(" the about page")
+    res.sendFile(`${__dirname}/calc.html`)
 ));
 
+// now if we want to create one more path
+
+// app.get("/about", (req,res) =>(
+//     res.send(" the about page")
+// ));
+
 // now to create a server we use..
-app.listen(5000, () =>{
+app.listen(9000, () =>{
     console.log("server's created");
 });
