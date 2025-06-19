9b74cdcfe5873b4f12aaa1128396c5eae6383da7
Megha <jangidmegha2002@gmail.com>
2025-06-19 09:09:35 +0530

    react link

--- photo_sharing/src/users/components/Useritem.js
+++ photo_sharing/src/users/components/Useritem.js
@@ -1,22 +1,25 @@
 import React from "react";
+import {Link} from "react-router-dom";
+
 import "./UsersItem.css";
 
 const Useritem =(props) =>{
-
+    
     return(
             <li className="useritem" >
 
                     <div className="useritem-content" >
 
+                        <Link to = {`${props.id}/locations`} >
                         <div className="useritem-pic" >
-                            <img src={props.pic} alt="" />
+                            <img src={props.pic} alt="user-photo" />
                         </div>
 
                         <div className="useritem-info" >
                         <h2>{props.name}</h2>
-                        <h3>{props.nol}</h3>
-                        {props.nol===1 ? "location" :"locations"}
+                        <h3>{props.nol} {props.nol===1 ? "location" :"locations"}</h3>
                         </div>
+                        </Link>
 
                     </div>
 
