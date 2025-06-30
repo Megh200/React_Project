034a8f002df07003ab947c8d6554d80de28dd873
Megha <jangidmegha2002@gmail.com>
2025-06-30 20:31:58 +0530

    ..

--- photo_sharing/Backened/routes/locations_route.js
+++ photo_sharing/Backened/routes/locations_route.js
@@ -3,28 +3,7 @@ const Myerror = require("../model/error");
 
 const loc_router = express.Router();     // for path
 
-const user_locations = [
-        {id:"loc1",
-        title:"albert hall museum",
-        desc:"The Albert Hall Museum in Jaipur is the oldest museum of the state and functions as the state museum of Rajasthan.",
-        address:"Ram Niwas Garden, Ashok Nagar, Jaipur, Rajasthan 302007",
-        userid:"1"
-        },
 
-        {id:"loc2",
-        title:"delhi",
-        desc:"..",
-        address:"...",
-        userid:"1"
-        },
-
-        {id:"loc3",
-        title:"red fort",
-        desc:"...",
-        address:"...",
-        userid:"2"
-        }
-    ];
 
 loc_router.get("/:locid", );
 
