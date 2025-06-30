797290aeb31f220cb4e53ee73a8e482ee3cc4e09
Megha <jangidmegha2002@gmail.com>
2025-07-01 00:20:00 +0530

    ..

---
+++ photo_sharing/src/users/components/Userslist.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Userslist = (props) =>{
+
+    return (
+        <div>
+            <p>hello</p>
+            <img src={props.arr[0].pic} width="350px" height="250px" />
+        </div>
+    )
+
+};
+
+export default Userslist;
