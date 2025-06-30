3b4af5b9bf57eee40b75b0a28277c5b8e28fe292
Megha <jangidmegha2002@gmail.com>
2025-07-01 00:19:23 +0530

    data added for users

--- photo_sharing/src/users/pages/Users.js
+++ photo_sharing/src/users/pages/Users.js
@@ -1,10 +1,21 @@
 import React from "react";
-
+import Userslist from "../components/Userslist.js";
+;
 const Users = () =>{
+
+    const all_users = [
+            {id:1, name:"tiya", pic:"https://i.pinimg.com/736x/6a/8a/75/6a8a75997108f805112355c48ca4af27.jpg", nol:3},
+            {id:1, name:"tiya", pic:"https://i.pinimg.com/736x/6a/8a/75/6a8a75997108f805112355c48ca4af27.jpg", nol:5}
+        ]
 
 return (
     <div>
         <p>users page</p>
+
+        <Userslist arr={all_users} />
+
+
+
     </div>
 
 );
