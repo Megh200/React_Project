d84a99aa1d3ae187a453629a2d7848292a9dc9a6
Megha <jangidmegha2002@gmail.com>
2025-07-05 09:24:53 +0530

    ..

--- photo_sharing/src/users/pages/Users.js
+++ photo_sharing/src/users/pages/Users.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Userslist from "../components/Userslist.js";
-;
+
+
 const Users = () =>{
 
     const all_users = [
-            {id:1, name:"tiya", pic:"https://i.pinimg.com/736x/6a/8a/75/6a8a75997108f805112355c48ca4af27.jpg", nol:3},
-            {id:2, name:"arya", pic:"https://i.pinimg.com/736x/6a/8a/75/6a8a75997108f805112355c48ca4af27.jpg", nol:1}
+            {id:"1", name:"tiya", pic:"https://i.pinimg.com/736x/6a/8a/75/6a8a75997108f805112355c48ca4af27.jpg", nol:3},
+            {id:"2", name:"arya", pic:"https://i.pinimg.com/736x/6a/8a/75/6a8a75997108f805112355c48ca4af27.jpg", nol:1}
         ]
 
 return (
