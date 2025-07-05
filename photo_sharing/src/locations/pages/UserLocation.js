2e6a8f588900b0f830fe8dd8c158fbe18369c102
Megha <jangidmegha2002@gmail.com>
2025-07-05 09:35:57 +0530

    improve the code

--- photo_sharing/src/locations/pages/UserLocation.js
+++ photo_sharing/src/locations/pages/UserLocation.js
@@ -7,7 +7,7 @@ const Userlocation = (props) =>{
 
     const user_locations = [
         {id:"loc1",
-        tittle:"albert hall museum",
+        title:"albert hall museum",
         pic:"https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcRXT95DcGUEpc3qRxBk1DxeTBrtxYuyXNDL-fmNphvPOgvRE3HY0KN2_R03_tIN79Sp",
         desc:"The Albert Hall Museum in Jaipur is the oldest museum of the state and functions as the state museum of Rajasthan.",
         address:"Ram Niwas Garden, Ashok Nagar, Jaipur, Rajasthan 302007",
@@ -15,7 +15,7 @@ const Userlocation = (props) =>{
         },
 
         {id:"loc2",
-        tittle:"delhi",
+        title:"delhi",
         pic:"https://www.shutterstock.com/image-photo/india-gate-sunrise-rajpath-new-600nw-2453461857.jpg",
         desc:"..",
         address:"...",
@@ -23,7 +23,7 @@ const Userlocation = (props) =>{
         },
 
         {id:"loc3",
-        tittle:"red fort",
+        title:"red fort",
         pic:"https://lh3.googleusercontent.com/gps-cs-s/AC9h4nrtBL79Rr8KB-Ft1VgTGmyajsXBHTvNIe2QFoKSQDaJ3jfU4Kmc3ya1DP6LXZFHntRspoqzL7wlxercsIlJ8qy7nZtt63_FRyeyo6x0tSPYIUKz9lJYXi7zKCBug6KnZ15FDuhC9Q=w270-h312-n-k-no",
         desc:"...",
         address:"...",
