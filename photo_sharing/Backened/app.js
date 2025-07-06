6c95c77465a4db8b043b6df9d74ad9b107890504
Megha <jangidmegha2002@gmail.com>
2025-07-06 23:58:26 +0530

    ...

--- photo_sharing/Backened/app.js
+++ photo_sharing/Backened/app.js
@@ -10,12 +10,15 @@ const app = express();
 app.use("/api/loc", loc_route);         // but now gave the path for locations
 
 app.use((error, req, res, next) =>{
-    if(res.headerSent){
+    if(res.headersSent){
          next(error);
     }
     res.status(error.errorcode || 500);
-    res.json({result:"fail", msg:error.message || "smth bad"});
+    res.json({result:"fail", msg:error.message }); // NOT msg:error.msg
 })
+// JavaScript’s built-in Error class uses the .message property to store the error message
+// bcz Built-in Error class only gives you these properties-> .message, .name, .stack etc but
+// JavaScript’s built-in Error class does not include .errorcode, .errorCode, or any such property by default.
 
 app.listen(5000,() =>{
     console.log("server is created");
