769240731c0da3d6b4d5b0bddc78d2e1bee90918
Megha <jangidmegha2002@gmail.com>
2025-07-08 09:42:39 +0530

    button added in form in loc file

--- photo_sharing/src/locations/pages/newloc.js
+++ photo_sharing/src/locations/pages/newloc.js
@@ -17,7 +17,10 @@ return (
         </div>
         <div className="form-control" >
             <label>Address</label>
-            <input type="text" name="Address" required />
+            <input type="text" rows="3" name="Address" required />
+        </div>
+         <div className="form-control" >
+            <button>Submit</button>
         </div>
     </form>
 );
