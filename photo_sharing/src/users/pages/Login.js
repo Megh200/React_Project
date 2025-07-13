04470cbf57aeec17db00664b5853108099309e96
Megha <jangidmegha2002@gmail.com>
2025-07-13 23:49:53 +0530

    ....

--- photo_sharing/src/users/pages/Login.js
+++ photo_sharing/src/users/pages/Login.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Login.css";
 
 const Login = () =>{
-    const submithandler = () =>{};
+    
+    const[logindata,setlog] = useState(
+        {
+            email:"",
+            pass:""
+        }
+    );
 
-    const changehandler = () =>{};
+    const submithandler = (event) =>{
+        event.preventDefault();
+        console.log("logindata", logindata);
+    };
+
+    const changehandler = (event) =>{
+        const newentry = event.target.value;
+        const inputname = event.target.name;
+
+        setlog((previousvalue)=>{
+            if(inputname === "loginemail"){
+                return({
+                    email:newentry,
+                    pass:previousvalue.pass
+                })
+            }
+            else if(inputname === "loginpass"){
+                return({
+                    email:previousvalue.email,
+                    pass:newentry
+                })
+            }
+        }
+
+        );
+
+    };
 
 
 return(
