ab05413364e142069206af78cb362cff91a61628
Megha <jangidmegha2002@gmail.com>
2025-07-16 00:17:37 +0530

    routes path & other components added

--- photo_sharing/src/App.js
+++ photo_sharing/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Users from "./users/pages/Users.js";
 import Newloc from "./locations/pages/Newloc.js";
 
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 
 
 const App = () => {
@@ -11,16 +11,32 @@ const App = () => {
     return(
       
     <div>
+      {/* route,redirect,switch etc.. are all router's components */}
+
       <Router>
+
+        <Switch>
+
         {/* created a default home page eg: picpot/ , that will give users liston frontend webpage */}
-        <Route path = "/" >        
+        <Route path = "/" exact>        
             <Users/>
         </Route>
 
-        <Route path = "/loc/new">
+        <Route path = "/loc/new" exact>
         <Newloc/>
         </Route>
-        
+      {/* EXACT is used bcz when we run /loc/new, it shows users content also with that & we don't want it */}
+
+      <Redirect to="/" />
+      {/* if we gave any useless path,it will redirect to userspage(default/home page) */}
+
+      </Switch>
+
+
+
+
+
+
       </Router>
 
         
