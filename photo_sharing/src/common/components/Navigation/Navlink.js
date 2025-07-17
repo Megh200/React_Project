99562e380dad6c92b5b67be6aa31723935b090f3
Megha <jangidmegha2002@gmail.com>
2025-07-17 19:51:03 +0530

    ..

--- photo_sharing/src/common/components/Navigation/Navlink.js
+++ photo_sharing/src/common/components/Navigation/Navlink.js
@@ -1,23 +1,41 @@
-import React from "react";
+import React, {useContext} from "react";
 import {NavLink} from "react-router-dom";
 
 import "./Navlink.css";
+import { LoginContext } from "../context.js";
 
 const Navlink = (props) =>{
+
+    // to use LoginContext, we'll use hook "useContext" here
+    // bcz we have to share login info now here 
+    const islogin = useContext(LoginContext);        // since Logincontext is obj so islogin is also obj
+
+
     return (
         <ul className="navlinks" >
             <li>
                 <NavLink to="/" exact>Users</NavLink>
             </li>
-            <li>
+
+            {/* we want "my loca.." & "add loc.." to be shown when loggedin;
+            ofcourse "Users" will be shown everytime */}
+
+            { islogin.isLoggedin && 
+            (<li>
                 <NavLink to="/1/locations" >My locations</NavLink>
-            </li>
-            <li>
+            </li>) }
+            
+            { islogin.isLoggedin && 
+            (<li>
                 <NavLink to="/loc/new" >Add locations</NavLink>
-            </li>
-            <li>
+            </li>) }
+
+            { !islogin.isLoggedin &&
+            (<li>
                 <NavLink to="/Login" >SignIn/Up</NavLink>
-            </li>
+            </li>)}
+
+            { islogin.isLoggedin && ( <button>Logout</button> ) }
             
         </ul>
     );
