b228288d76f03877f16fa07250285695a797a091
Megha <jangidmegha2002@gmail.com>
2025-07-21 21:57:59 +0530

    ..

--- photo_sharing/src/locations/pages/newloc.js
+++ photo_sharing/src/locations/pages/newloc.js
@@ -1,9 +1,25 @@
 import React from "react";
 
+import "./Newloc.css";
+
 const Newloc = () =>{
 
 return (
-    <h1>locations of users are here </h1>
+    // <h1>locations of users are here </h1>
+    <form className="location-form" >
+        <div className="form-control" >
+            <label>Title</label>
+            <input type="text" name="newloctitle" required />
+        </div>
+        <div className="form-control" >
+            <label>Description</label>
+            <input type="text" name="newlocdesc" required />
+        </div>
+        <div className="form-control" >
+            <label>Address</label>
+            <input type="text" name="Address" required />
+        </div>
+    </form>
 );
 
 };
