2bf3d1544d62e8f64a1559a68ab112b94ec2df1f
Megha <jangidmegha2002@gmail.com>
2025-07-22 10:01:52 +0530

    useState in file

--- photo_sharing/src/locations/pages/newloc.js
+++ photo_sharing/src/locations/pages/newloc.js
@@ -1,23 +1,38 @@
 import React from "react";
+import {useState} from "react";
 
 import "./Newloc.css";
 
 const Newloc = () =>{
+    const [newlocation,setnewlocation] = useState(
+        {title:'',
+         desc:'',
+         address:''
+        }
+    );
+
+    const submithandler = (event) =>{
+        event.preventDefault();
+    };
+
+    const changehandler = () =>{
+
+    };
 
 return (
     // <h1>locations of users are here </h1>
-    <form className="location-form" >
+    <form className="location-form" onSubmit={submithandler} >
         <div className="form-control" >
             <label>Title</label>
-            <input type="text" name="newloctitle" required />
+            <input type="text" name="newloctitle" required onChange={changehandler} />
         </div>
         <div className="form-control" >
             <label>Description</label>
-            <textarea></textarea>
+            <textarea rows="3" name="newlocdesc" onChange={changehandler} ></textarea>
         </div>
         <div className="form-control" >
             <label>Address</label>
-            <input type="text" rows="3" name="Address" required />
+            <input type="text" name="newlocAddress" required onChange={changehandler} />
         </div>
          <div className="form-control" >
             <button>Submit</button>
