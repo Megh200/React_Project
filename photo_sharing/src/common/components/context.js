2fcf5894319ce261fcf6506b02581cd6351c3648
Megha <jangidmegha2002@gmail.com>
2025-07-25 19:18:03 +0530

    ..

--- photo_sharing/src/common/components/context.js
+++ photo_sharing/src/common/components/context.js
@@ -3,9 +3,9 @@ import { createContext } from "react";
 // a small function that's why exported directly
 export const LoginContext = createContext(
     // since "createContext" is a hook method so there is an object in it that is
-    // a var. , two functions 
+    // a var. & two functions 
     {
-        isloggedin:false,  // we are not login now so falset
+        isloggedin:false,  // we are not login now so false
         login: () =>{},
         logout: () =>{}
     }
