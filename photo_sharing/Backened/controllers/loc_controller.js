ecd0a95542c2bbbd65d67a519efc44b74e211dd0
Megha <jangidmegha2002@gmail.com>
2025-07-30 20:28:29 +0530

    ...

--- photo_sharing/Backened/controllers/loc_controller.js
+++ photo_sharing/Backened/controllers/loc_controller.js
@@ -1,8 +1,33 @@
 
 
 
-const getlocbylocid = (req, res, next) =>{};
+const getlocbylocid = (req, res, next) =>{
+     // url locid by params of useParams
+    const locid = req.params.locid;
+    const location = user_locations.find(loc =>{
+        return (loc.id===locid) });
+    // console.log(location);
 
-const getlocbyuid = (req, res, next) =>{};
+    if(!location){
+        // error
+        return next(new Myerror("no locations of this id", 404));
+    }
+    
+    res.status(200).json({result:"success", msg:location });
+};
 
-const createnewloc = (req, res, next) =>{};
+const getlocbyuid = (req, res, next) =>{
+     const uid = req.params.uid;
+    const users = user_locations.filter(user =>{
+        return (user.userid===uid)
+    });
+    res.status(200).json({result:"success", msg:users });
+};
+
+const createnewloc = (req, res, next) =>{
+    const {title, desc, address, userid} = req.body;
+    const newloc = {title, desc, address, userid};
+
+    user_locations.push(newloc);
+    res.status(201).json({result:"success", msg:newloc});
+};
