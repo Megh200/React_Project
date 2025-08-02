11612164989434221be76078cabcee4ee97436fa
Megha <jangidmegha2002@gmail.com>
2025-08-02 18:36:43 +0530

    code to use api

--- photo_sharing/Backened/practice/api.js
+++ photo_sharing/Backened/practice/api.js
@@ -1,3 +1,21 @@
 const express = require("express");
 
-const axios = 
+const axios = require("axios");   // an api tool
+
+const api = express();
+
+// to use type.fit web data with api
+axios({
+    method:"get",
+    url:"https://type.fit/api/quotes",
+}).then(
+(response) =>{
+    console.log(response) },
+(error) =>{
+    console.log(error) }
+);
+
+
+api.listen(5000, () =>{
+    console.log("ok")
+});
