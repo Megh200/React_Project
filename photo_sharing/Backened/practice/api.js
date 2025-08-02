68407cfc7e54e01a9b05ebb196051a880dd1d6a0
Megha <jangidmegha2002@gmail.com>
2025-08-02 18:43:39 +0530

    added an api

--- photo_sharing/Backened/practice/api.js
+++ photo_sharing/Backened/practice/api.js
@@ -7,7 +7,7 @@ const api = express();
 // to use type.fit web data with api
 axios({
     method:"get",
-    url:"https://type.fit/api/quotes",
+    url:"https://v6.exchangerate-api.com/v6/51f63df4568e1d049f3c3332/pair/EUR/GBP",
 }).then(
 (response) =>{
     console.log(response) },
