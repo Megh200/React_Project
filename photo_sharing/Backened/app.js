04360eae65638ad0b418b33eb3a3abd77b3c8e44
Megha <jangidmegha2002@gmail.com>
2025-08-03 20:29:43 +0530

    error handling

--- photo_sharing/Backened/app.js
+++ photo_sharing/Backened/app.js
@@ -13,8 +13,8 @@ app.use((error, req, res, next) =>{
     if(res.headerSent){
          next(error);
     }
-    res.status(error.code || 500);
-    res.json({result:"fail", msg:error.msg || "smth bad"});
+    res.status(error.errorcode || 500);
+    res.json({result:"fail", msg:error.message || "smth bad"});
 })
 
 app.listen(5000,() =>{
