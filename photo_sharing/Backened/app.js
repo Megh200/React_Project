13fd7ab184a075babccff4d3eaef1997cfd2f824
Megha <jangidmegha2002@gmail.com>
2025-08-03 20:01:20 +0530

    ...

--- photo_sharing/Backened/app.js
+++ photo_sharing/Backened/app.js
@@ -4,7 +4,8 @@ const loc_route = require("./routes/locations_route");
 const app = express();
 
 
-app.use(loc_route);
+// app.use(loc_route);     in the beginning,we did this
+app.use("/api/loc", loc_route);         // but now gave the path for locations
 
 app.listen(5000,() =>{
     console.log("server is created");
