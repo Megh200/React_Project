830ac56df2237c77de4587d60be7d5809b52e38b
Megha <jangidmegha2002@gmail.com>
2025-08-04 21:43:31 +0530

    ...

--- photo_sharing/Backened/routes/locations_route.js
+++ photo_sharing/Backened/routes/locations_route.js
@@ -2,7 +2,8 @@ const express = require('express');
 const loc_router = express.Router();     // for path
 
 
-loc_router.get("/", (req, res, next) =>{
+loc_router.get("/:locid", (req, res, next) =>{
+    
     res.status(200).json({result:"success", msg:"loc_route" });
 });
 
