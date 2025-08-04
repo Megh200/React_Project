c41a0b6f1fa4665d8167e5babb4a35e95b8befa1
Megha <jangidmegha2002@gmail.com>
2025-08-04 23:12:53 +0530

    params state code

--- photo_sharing/Backened/routes/locations_route.js
+++ photo_sharing/Backened/routes/locations_route.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const loc_router = express.Router();     // for path
 
+const user_locations = [
+        {id:"loc1",
+        title:"albert hall museum",
+        desc:"The Albert Hall Museum in Jaipur is the oldest museum of the state and functions as the state museum of Rajasthan.",
+        address:"Ram Niwas Garden, Ashok Nagar, Jaipur, Rajasthan 302007",
+        userid:"1"
+        },
+
+        {id:"loc2",
+        title:"delhi",
+        desc:"..",
+        address:"...",
+        userid:"1"
+        },
+
+        {id:"loc3",
+        title:"red fort",
+        desc:"...",
+        address:"...",
+        userid:"2"
+        }
+    ];
 
 loc_router.get("/:locid", (req, res, next) =>{
+    const locid = req.params.locid;     // url locid by params of useParams
+    const location = user_locations.find(loc =>{
+        return (loc.id===locid) });
+
     
-    res.status(200).json({result:"success", msg:"loc_route" });
+    res.status(200).json({result:"success", msg:location });
 });
 
 module.exports = loc_router;
