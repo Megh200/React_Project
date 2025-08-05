7490671f46364c0d7b426702dfcc2b39344497f2
Megha <jangidmegha2002@gmail.com>
2025-08-06 00:31:18 +0530

    routes code

---
+++ photo_sharing/Backened/routes/locations_route.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const loc_router = express.Router();     // for path
+
+
+loc_router.get("/", (req, res, next) =>{
+    res.status(200).json({result:"success", msg:"loc_route" });
+});
+
+module.exports = loc_router;
