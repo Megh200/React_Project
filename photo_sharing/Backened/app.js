aa3595eb95ae282d29adcbbe15afc3923d9117e0
Megha <jangidmegha2002@gmail.com>
2025-08-07 18:44:51 +0530

    ...error code

--- photo_sharing/Backened/app.js
+++ photo_sharing/Backened/app.js
@@ -7,6 +7,12 @@ const app = express();
 // app.use(loc_route);     in the beginning,we did this
 app.use("/api/loc", loc_route);         // but now gave the path for locations
 
+app.use((error, req, res, next) =>{
+    if(res.headerSent){
+        // error
+    }
+})
+
 app.listen(5000,() =>{
     console.log("server is created");
 });
