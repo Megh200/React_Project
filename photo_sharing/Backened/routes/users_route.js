b639bb939060195cd9be15a6eace2c5c299f700a
Megha <jangidmegha2002@gmail.com>
2025-08-12 01:10:29 +0530

    ...

--- photo_sharing/Backened/routes/users_route.js
+++ photo_sharing/Backened/routes/users_route.js
@@ -4,7 +4,7 @@ const Myerror = require("../model/error");
 
 const user_router = express.Router();
 
-user_router.get("/:uid", user_controller.getuserbyuid);
+// user_router.get("/:uid", user_controller.getuserbyuid);
 
 user_router.get("/", user_controller.getusers);
 
