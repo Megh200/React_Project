a96f908e3b030d0a2375a8610b134fee4240b60a
Megha <jangidmegha2002@gmail.com>
2025-08-16 22:23:12 +0530

    ...

--- photo_sharing/Backened/app.js
+++ photo_sharing/Backened/app.js
@@ -12,6 +12,9 @@ app.use(express.json());
 app.use("/api/loc", loc_route);         // but now gave the path for locations      a route middleware
 app.use("/api/users", user_route);
 
+app.use("*", (req, res, next) =>{
+    return next(new Myerror("can't find path",404));
+});
 
 app.use((error, req, res, next) =>{                  // error middleware
     if(res.headersSent){
