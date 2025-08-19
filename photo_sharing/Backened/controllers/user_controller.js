4352cd5fbb988cf02e9e0d0251cbfe0951ad824e
Megha <jangidmegha2002@gmail.com>
2025-08-20 00:43:00 +0530

    getusers code

--- photo_sharing/Backened/controllers/user_controller.js
+++ photo_sharing/Backened/controllers/user_controller.js
@@ -5,14 +5,16 @@ const all_users = [
             {id:"2", name:"arya", pic:"https://i.pinimg.com/736x/6a/8a/75/6a8a75997108f805112355c48ca4af27.jpg", nol:1}
         ]
 
-exports.getuserbyuid = (req, res, next) =>{
-    const uid = req.params.uid;
-    const user = all_users.find(user => user.id===uid);
+// exports.getuserbyuid = (req, res, next) =>{
+//     const uid = req.params.uid;
+//     const user = all_users.find(user => user.id===uid);
 
-    if(!user){
-        return next(new Myerror("no such userid", 404));
-    }
+//     if(!user){
+//         return next(new Myerror("no such userid", 404));
+//     }
 
-    res.status(200).json({result:"success", msg:user});
-}
+//     res.status(200).json({result:"success", msg:user});
+// }
 
+
+exports.getusers
