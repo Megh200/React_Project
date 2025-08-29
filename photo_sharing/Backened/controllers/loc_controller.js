f5fc51ce1f88e1ca40c52f92f24e9101383e46f2
Megha <jangidmegha2002@gmail.com>
2025-08-29 18:03:59 +0530

    ..

--- photo_sharing/Backened/controllers/loc_controller.js
+++ photo_sharing/Backened/controllers/loc_controller.js
@@ -1,27 +1,30 @@
 const Myerror = require("../model/error");
+const locschema = require("../model/location");
 
-let user_locations = [
-        {id:"loc1",
-        title:"albert hall museum",
-        desc:"The Albert Hall Museum in Jaipur is the oldest museum of the state and functions as the state museum of Rajasthan.",
-        address:"Ram Niwas Garden, Ashok Nagar, Jaipur, Rajasthan 302007",
-        userid:"1"
-        },
+// dummy data
+// let user_locations = [
+//         {id:"loc1",
+//         title:"albert hall museum",
+//         desc:"The Albert Hall Museum in Jaipur is the oldest museum of the state and functions as the state museum of Rajasthan.",
+//         address:"Ram Niwas Garden, Ashok Nagar, Jaipur, Rajasthan 302007",
+//         userid:"1"
+//         },
 
-        {id:"loc2",
-        title:"delhi",
-        desc:"..",
-        address:"...",
-        userid:"1"
-        },
+//         {id:"loc2",
+//         title:"delhi",
+//         desc:"..",
+//         address:"...",
+//         userid:"1"
+//         },
 
-        {id:"loc3",
-        title:"red fort",
-        desc:"...",
-        address:"...",
-        userid:"2"
-        }
-    ];
+//         {id:"loc3",
+//         title:"red fort",
+//         desc:"...",
+//         address:"...",
+//         userid:"2"
+//         }
+//     ];
+
 
 // to separately or individually export them .....
 
